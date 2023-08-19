3f32e44a73c3278266e001e351539d1f3e8c088d
Mert patlar <meertpatlar@gmail.com>
2023-08-20 02:21:13 +0300

    favicon added

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
 
 
-from .models import Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": [
+         "pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
